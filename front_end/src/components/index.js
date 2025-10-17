705224367787cd1a739e0ded942921a88ecdbe04
Rafael Gabardo <rggabardo@gmail.com>
2025-10-17 10:51:24 -0300

    Começando novos componentes e preparando para conectar ao banco de dados

---
+++ front_end/src/components/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export function Index() {
+  return (
+    <section>
+      <h1>Index Component</h1>
+    </section>
+  );
+}
