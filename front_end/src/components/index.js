96691eabc15712f4b531be126e883dd1484af3e7
Rafael Gabardo <rggabardo@gmail.com>
2025-10-20 16:30:38 -0300

    Configurando o bootstrap e começando o front-end

--- front_end/src/components/index.js
+++ front_end/src/components/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Footer } from "./footer";
+import { NavBar } from "./navbar";
 
 export function Index() {
   return (
--- front_end/src/components/navbar.js
+++ front_end/src/components/navbar.js
@@ -2,5 +2,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 
 export function NavBar() {
-  return;
+  return (
+    <nav className='container-fluid'>
+      <ul>
+        
+      </ul>
+    </nav>
+  );
 }
